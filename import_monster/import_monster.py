122bb95e1f913b435d859ce628556d5f35815a3c
Polina Fokina <pfokina@gmail.com>
2021-04-11 23:30:14 +0500

    Migrate tests from prints to pytest

--- import_monster/import_monster.py
+++ import_monster/import_monster.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-import builtins
-import collections
-import email.mime
 import importlib
-import sys
 from types import ModuleType
 from typing import Callable, List, Union
 
@@ -27,30 +23,3 @@ def methods_importer(method_name: str, modules: List[Union[str, ModuleType]]) ->
             continue
 
     return callable_objects
-
-
-if __name__ == "__main__":
-    print(methods_importer("__init__", ["builtins", "collections", "email"]))
-    # [<method-wrapper '__init__' of module object at 0x7fc4fcd6b860>,
-    # <method-wrapper '__init__' of module object at 0x7fc4fe037ea0>,
-    # <method-wrapper '__init__' of module object at 0x7fc4fe06a040>]
-
-    print(methods_importer("__init__", [builtins, collections, email]))
-    # [<method-wrapper '__init__' of module object at 0x7fc4fcd6b860>,
-    # <method-wrapper '__init__' of module object at 0x7fc4fe037ea0>,
-    # <method-wrapper '__init__' of module object at 0x7fc4fe06a040>]
-
-    print(methods_importer("deque", ["collections"]))
-    # [<class 'collections.deque'>]
-
-    print(methods_importer("deque", [collections]))
-    # [<class 'collections.deque'>]
-
-    print(methods_importer("text", [email.mime]))
-    # should be: class email.parser
-    # why doesn't work?
-
-    print(sys.meta_path)
-    # [<class '_frozen_importlib.BuiltinImporter'>,
-    # <class '_frozen_importlib.FrozenImporter'>,
-    # <class '_frozen_importlib_external.PathFinder'>]
---
+++ tests/test_import_monster.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import builtins
+import collections
+import math
+
+from import_monster import methods_importer
+
+
+def test_methods_importer_imported_module_callable():
+    result = methods_importer("deque", [collections])
+    assert result == [collections.deque]
+
+
+def test_methods_importer_imported_module_not_callable():
+    result = methods_importer("pi", [math])
+    print("result: ", result)
+    assert result == []
+
+
+def test_methods_importer_string_module():
+    result = methods_importer("deque", ["collections"])
+    assert result == [collections.deque]
+
+
+def test_methods_importer_multiply_strings():
+    result = methods_importer("__init__", ["builtins", "collections", "math"])
+    assert result == [builtins.__init__, collections.__init__, math.__init__]
+
+
+def test_methods_importer_multiply_modules():
+    result = methods_importer("__init__", [builtins, collections, math])
+    assert result == [builtins.__init__, collections.__init__, math.__init__]
