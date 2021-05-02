278770beaef83e03cd9c07073b884128044b87af
Polina Fokina <pfokina@gmail.com>
2021-05-02 23:43:17 +0500

    More tests

--- reply_for_remarks.md
+++ reply_for_remarks.md
@@ -3,6 +3,6 @@
 1. What's wrong with my __init__.py? Example i looked into: https://github.com/girafe-ai/msai-python/blob/master/week1_02_github_actions/actions-example/my_awesome_package/__init__.py
 2. Here is it: https://github.com/CleverSquirrel/mipt_python/blob/main/.pre-commit-config.yaml
 3. Here they are: https://github.com/CleverSquirrel/mipt_python/blob/main/Makefile#L47
-4. TODO
+4. Done
 5. I changed my import_monster, now there is a class-container for methods_importer function
 6. Fixed
--- tests/test_import_monster.py
+++ tests/test_import_monster.py
@@ -3,6 +3,8 @@ import builtins
 import collections
 import math
 
+import pytest
+
 from import_monster import MethodsImporter
 
 
@@ -29,3 +31,18 @@ def test_methods_importer_multiply_strings():
 def test_methods_importer_multiply_modules():
     result = MethodsImporter.methods_importer("__init__", [builtins, collections, math])
     assert result == [builtins.__init__, collections.__init__, math.__init__]
+
+
+def test_methods_importer_module_and_string():
+    result = MethodsImporter.methods_importer("__init__", ["collections", math])
+    assert result == [collections.__init__, math.__init__]
+
+
+def test_methods_importer_nonexistent_module():
+    result = MethodsImporter.methods_importer("__init__", ["collections", "nonexistent"])
+    assert result == [collections.__init__]
+
+
+def test_methods_importer_incorrect_module_type():
+    with pytest.raises(TypeError):
+        MethodsImporter.methods_importer("test", [777])  # 1 type is not ModuleType or str
